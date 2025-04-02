4a6c1f7ce64f464a136edef4487018a6d15d9d56
gfragkos80 <g.frangos80@gmail.com>
2025-04-02 19:48:38 -0400

    add more CSS code

---
+++ travel_recommendation.js
@@ -0,0 +1,9 @@
+const addResetButton = document.getElementById("btnReset");
+
+const btnSearch = document.getElementById('btnSearch');
+
+const btnSubmit = document.getElementById('submitBtn');
+
+const countries = [];
+
+
